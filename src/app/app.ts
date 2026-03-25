d40c401fafe91f0459bb2e3c8413d4ef1ab0ae33
Adil Qureshi <adilqureshi.dev@gmail.com>
2026-03-26 01:47:56 +0300

    add services section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 import { Footer } from './footer/footer';
+import { Services } from "./services/services";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, Services],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
