0fdbf3f9e1af6f160788c204e07a6b2d0ab5b131
Adil Qureshi <adilqureshi.dev@gmail.com>
2026-04-10 16:29:52 +0300

    update code contact

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import { Footer } from './footer/footer';
 import { Services } from "./services/services";
 import { About } from "./about/about";
 import { Team } from "./team/team";
+import { Contact } from "./contact/contact";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, Services, About, Team],
+  imports: [RouterOutlet, Header, Footer, Services, About, Team, Contact],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
